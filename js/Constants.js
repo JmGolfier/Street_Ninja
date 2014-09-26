9fd69b9f81a033e64f2ac6567b53f4e1cfcd4d98
epayet <jakspinning@gmail.com>
2014-09-26 14:37:41 +0200

    Changed constants

--- js/Constants.js
+++ js/Constants.js
@@ -8,12 +8,12 @@ module.exports = {
 
     Paths: {
         Physijs: {
-            Worker: '/lazy_load_libs/physijs_worker.js',
-            Ammo: '/lazy_load_libs/ammo.js'
+            Worker: 'lazy_load_libs/physijs_worker.js',
+            Ammo: 'ammo.js'
         },
 
         Models: {
-            Ninja: "/assets/models/estj-dino-anim.json"
+            Ninja: "assets/models/estj-dino-anim.json"
         }
     }
 };
